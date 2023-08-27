ffc34b95cb838cbd862ec66c1fc71824a54c7ff1
mbunix <bellianotieno24@gmal.com>
2023-08-27 18:14:59 +0300

    Fix :Validations

---
+++ Task Manager/Helpers/ValidationScheme.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task_Manager.Helpers
+{
+    public class ValidationScheme
+    {
+        public  bool  validateInput(string UserName, string Password)
+        {
+            if(String.IsNullOrEmpty(UserName)|| String.IsNullOrEmpty(Password)){
+                Console.WriteLine("Username or Password cannot be empty");
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValiateUserInDatabase(string UserName, string Password)
+        {
+            var user = DbContext.User.Where(u => u.UserName == UserName && u.Password == Password).FirstOrDefault();
+            if(user == null){
+                Console.WriteLine("User does not exist");
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidateAdminInDatabase(string UserName, string Password)
+        {
+            var admin = DbContext.Admin.Where(u => u.UserName == UserName && u.Password == Password).FirstOrDefault();
+            if(admin == null){
+                Console.WriteLine("Admin does not exist");
+                return false;
+            }
+            return true;
+        }
+        
+    }
+}
---
+++ Task Manager/services/CreateProject.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Manager.services
+{
+    public class CreateProject
+    {
+        
+    }
+}
