be593ef4bbf52912663ea2ff1e28b93fd6334c19
Jimna <jimmynarokian254@gmail.com>
2023-08-28 01:13:15 +0300

    User Model updation and Admin services Interface creation

--- Task Manager/Helpers/ValidationScheme.cs
+++ Task Manager/Helpers/ValidationScheme.cs
@@ -9,7 +9,7 @@ namespace Task_Manager.Helpers
 {
     public class ValidationScheme
     {
-        public  bool  validateInput(string username, string password)
+        public  bool validateInput(string username, string password)
         {
             if(String.IsNullOrEmpty(username)|| String.IsNullOrEmpty(password)){
                 Console.WriteLine("Username or Password cannot be empty");
@@ -18,25 +18,27 @@ namespace Task_Manager.Helpers
             return true;
         }
 
-        //public bool ValiateUserInDatabase(string UserName, string Password)
-        //{
-        //    var user = DbContext.User.Where(u => u.UserName == UserName && u.Password == Password).FirstOrDefault();
-        //    if(user == null){
-        //        Console.WriteLine("User does not exist");
-        //        return false;
-        //    }
-        //    return true;
-        //}
+        public bool ValidateUserInDatabase(string UserName, string Password)
+        {
+            var user = DbContext.User.Where(u => u.UserName == UserName && u.Password == Password).FirstOrDefault();
+            if (user == null)
+            {
+                Console.WriteLine("User does not exist");
+                return false;
+            }
+            return true;
+        }
 
-        //public bool ValidateAdminInDatabase(string UserName, string Password)
-        //{
-        //    var admin = DbContext.Admin.Where(u => u.UserName == UserName && u.Password == Password).FirstOrDefault();
-        //    if(admin == null){
-        //        Console.WriteLine("Admin does not exist");
-        //        return false;
-        //    }
-        //    return true;
-        //}
-        
+        public bool ValidateAdminInDatabase(string UserName, string Password)
+        {
+            var admin = DbContext.Admin.Where(u => u.UserName == UserName && u.Password == Password).FirstOrDefault();
+            if (admin == null)
+            {
+                Console.WriteLine("Admin does not exist");
+                return false;
+            }
+            return true;
+        }
+
     }
 }
