c3d6b3d2ab260d63505b150d623d87f0504d2816
mbunix <bellianotieno24@gmal.com>
2023-08-27 14:08:34 +0300

    fix: Interfaces, Services,Tests

--- Task Manager/Models/User.cs
+++ Task Manager/Models/User.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Task_Manager.services;
 
 namespace Task_Manager.Models
 {
@@ -13,6 +13,40 @@ namespace Task_Manager.Models
 
         [PasswordPropertyText(true)]
         public string Password { get; set; } = "User";
+
+        public string Email { get; set; } = string.Empty;
+        public bool IsAdmin { get; set; } = false;
+
+
+
+        public User()
+        {
+
+        }
+ // USER METHODS 
+        public void Login( string userName, string password , string email, bool isAdmin)
+        {
+            this.UserName = userName;
+            this.Password = password;
+            this.Email = email;
+            this.IsAdmin = isAdmin;
+        }
+        
+
+       
+
+        public async Task ViewTask( string TaskId){
+            try {
+                var task = await DbContext.Tasks.FindAsync(TaskId);
+                 return task;
+            }catch {
+
+            }
+        }
+
+        public void MarkTaskAsComplete(){
+
+        }
 
 
 
